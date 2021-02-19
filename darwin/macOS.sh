432e2c725a406b02d76065852ac349cfac085f5d
Tianshu Wang <wang@tianshu.me>
2021-02-19 14:56:27 +0800

    Add ci.yml

--- darwin/macOS.sh
+++ darwin/macOS.sh
@@ -4,12 +4,6 @@
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
 
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until this script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 ###############################################################################
 # General UI/UX                                                               #
 ###############################################################################
--- darwin/setup.sh
+++ darwin/setup.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail # -e=-o errexit, -u=-o nounset
 
 # Ask for the administrator password upfront
-sudo -v
+sudo -n true 2>/dev/null || sudo -v
 # Keep-alive: update existing `sudo` time stamp until `init.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
