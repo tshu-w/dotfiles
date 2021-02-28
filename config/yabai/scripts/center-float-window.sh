5515b721a01bf39ecf7d8a1570db7b5b9fe00b40
Tianshu Wang <wang@tianshu.me>
2021-02-28 21:05:12 +0800

    Make center-float-window.sh work with multiple monitors

--- config/yabai/scripts/center-float-window.sh
+++ config/yabai/scripts/center-float-window.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-read -r W H <<< $(echo $(yabai -m query --displays --display | jq ".frame | .w, .h"))
+read -r X Y W H <<< $(echo $(yabai -m query --displays --display | jq ".frame | .x, .y, .w, .h"))
 read -r w h <<< $(echo $(yabai -m query --windows --window | jq ".frame | .w, .h"))
-x=$(((W - w) / 2))
-y=$(((H - 22 - h) / 2 + 22))
+x=$((((W - w) / 2)+X))
+y=$((((H - 22 - h) / 2 + 22)+Y))
 yabai -m window --move abs:$x:$y
