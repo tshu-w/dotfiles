fa2d4c4b01c827cc22273b6932d006bf1154ba49
Tianshu Wang <wang@tianshu.me>
2022-06-26 12:14:37 +0800

    Update setup.sh

--- darwin/setup.sh
+++ darwin/setup.sh
@@ -19,6 +19,9 @@ brew bundle -v --no-lock || true
 command -v yabai >/dev/null && brew services start yabai
 command -v skhd >/dev/null && brew services start skhd
 ln -sf /usr/local/opt/emacs-head@28/Emacs.app /Applications
+ln -s /usr/local/Caskroom/miniconda/base/ ~/.local/share/conda
+
+command -v wechattweak-cli && sudo wechattweak-cli install
 
 # mu init
 mu init -m $XDG_DATA_HOME/mail
