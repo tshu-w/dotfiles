19fcb44213cd77798b0302e88212063fa9cd3134
Tianshu Wang <wang@tianshu.me>
2023-09-15 09:25:08 +0800

    Fix center-float-window for floating-point values

--- config/yabai/scripts/center-float-window.sh
+++ config/yabai/scripts/center-float-window.sh
@@ -2,6 +2,6 @@
 
 read -r X Y W H <<< $(echo $(yabai -m query --displays --display | jq ".frame | .x, .y, .w, .h"))
 read -r w h <<< $(echo $(yabai -m query --windows --window | jq ".frame | .w, .h"))
-x=$((((W - w) / 2)+X))
-y=$((((H - 22 - h) / 2 + 22)+Y))
+x=$(echo "scale=2;($W - $w) / 2 + $X" | bc)
+y=$(echo "scale=2; ((($H - 22 - $h) / 2 + 22) + $Y)" | bc)
 yabai -m window --move abs:$x:$y
