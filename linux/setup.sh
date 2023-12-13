fbbf5509d4c818382f7f37768cd1ea0054d696da
Tianshu Wang <wang@tianshu.me>
2023-12-13 12:34:09 +0000

    Update linux setup.sh and Brewfile

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -euxo pipefail # -e=-o errexit, -u=-o nounset
 
-PREFIX=$XDG_DATA_HOME/linuxbrew
-if [ ! -d $PREFIX ]; then
-    git clone https://github.com/Homebrew/brew $PREFIX/Homebrew
-    mkdir $PREFIX/bin
-    ln -s $PREFIX/Homebrew/bin/brew $PREFIX/bin
+if [ -d /home/linuxbrew ]; then
+    PREFIX=/home/linuxbrew/.linuxbrew/
+    [ -d $PREFIX ] || \
+        curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /bin/bash
+else
+    PREFIX=$XDG_DATA_HOME/linuxbrew
+    if [ ! -d $PREFIX ]; then
+        git clone https://github.com/Homebrew/brew $PREFIX/Homebrew
+        mkdir $PREFIX/bin
+        ln -s $PREFIX/Homebrew/bin/brew $PREFIX/bin
+    fi
 fi
 eval $($PREFIX/bin/brew shellenv)
 
