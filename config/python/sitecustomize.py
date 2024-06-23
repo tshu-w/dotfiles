600c2a66a9a159babe9a1605a50948562bd6a092
Tianshu Wang <wang@tianshu.me>
2024-06-23 19:26:21 +0800

    Shrink tqdm default ncols

--- config/python/sitecustomize.py
+++ config/python/sitecustomize.py
@@ -3,3 +3,17 @@ try:
     traceback.install(show_locals=True)
 except ImportError:
     pass
+
+try:
+    import tqdm
+    from tqdm import tqdm as original_tqdm
+
+    class CustomTqdm(original_tqdm):
+        def __init__(self, *args, **kwargs):
+            if 'ncols' not in kwargs:
+                kwargs['ncols'] = 120
+            super().__init__(*args, **kwargs)
+
+    tqdm.tqdm = CustomTqdm
+except ImportError:
+    pass
