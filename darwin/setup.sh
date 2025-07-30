e55237cfc0491a5850e75caaaae8b3ca2ad26161
Tianshu Wang <wang@tianshu.me>
2025-07-30 11:57:19 +0800

    Migrate from Time Out to LookAway

--- darwin/setup.sh
+++ darwin/setup.sh
@@ -79,7 +79,7 @@ for dir in "fastmail" "iscas"; do
 done
 
 # Add login item
-for app in "AlDente" "Dash" "Dropbox" "Easydict" "Emacs" "Focus" "iTerm" "Ice" "LaunchBar" "Surge" "Time Out"; do
+for app in "AlDente" "Dash" "Dropbox" "Easydict" "Emacs" "Focus" "iTerm" "Ice" "LaunchBar" "LookAway" "Surge"; do
     osascript <<EOF
     tell application "System Events"
         make new login item at end with properties {} & ¬
